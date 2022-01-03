338cbf6ee5e93f9da31f616b4d89827e3aa4232e
myge <miguel.contreras1@mail.udp.cl>
2022-01-03 12:51:07 -0300

    a

---
+++ Tiare/1.py
@@ -0,0 +1,22 @@
+def dias_con_restriccion(digitos,ppu):
+
+    res = list()
+
+    digito = int(ppu[5])
+
+    for key in digitos.keys():
+        for number in digitos[key]:
+            if digito == number:
+                res.append(key)
+
+    return res
+
+digitos = {
+    'lunes' : [6,4,3],
+    'martes' : [6,1,3],
+    'lunes' : [6,8,3],
+    'lunes' : [6,4,1],
+
+}
+
+print(dias_con_restriccion(digitos,"ABCD56"))
